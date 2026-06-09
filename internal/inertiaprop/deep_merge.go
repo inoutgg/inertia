a0d418aad1f4d888ab037faf18bd6ef3306e190f
Roman <roman@vanesyan.com>
2026-06-09 13:39:05 +0000

    docs(internal): add comments to merge, deep_merge, and once opts

--- internal/inertiaprop/deep_merge.go
+++ internal/inertiaprop/deep_merge.go
@@ -2,14 +2,19 @@ package inertiaprop
 
 var _ Merge = (*DeepMergeOpts)(nil)
 
+// DeepMergeOpts opts a prop into deep merging. The matchOn values are sent to
+// the client as keys for deduplication when merging nested objects.
 type DeepMergeOpts struct {
 	matchOn []string
 }
 
+// NewDeepMergeOpts creates a DeepMergeOpts with the given matchOn keys.
 func NewDeepMergeOpts(matchOn ...string) *DeepMergeOpts {
 	return &DeepMergeOpts{matchOn}
 }
 
+// toMergeable returns a Mergeable with DeepMerge=true and the configured matchOn.
+// Nil receiver returns nil.
 func (o *DeepMergeOpts) toMergeable() *Mergeable {
 	if o == nil {
 		return nil
